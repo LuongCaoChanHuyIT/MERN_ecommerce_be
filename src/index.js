f0f8b16bba7d947f3c3dda54c91074ae6e05cd61
LuongCaoChanHuy <luongcaochanhuyitgmail.com>
2024-09-21 16:12:42 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+var cors = require("cors");
 const cookieParser = require("cookie-parser");
 dotenv.config();
 
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 const mongo_db = process.env.MONGO_DB;
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
