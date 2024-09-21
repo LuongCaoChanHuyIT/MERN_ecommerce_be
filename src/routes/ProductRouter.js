b4f164600544016984a66299eb904a54c3a3d527
LuongCaoChanHuy <luongcaochanhuyitgmail.com>
2024-09-21 16:55:51 +0700

    ok

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-var cors = require("cors");
+
 const ProductController = require("../controllers/ProductController.js");
 const {
   authMiddleware,
@@ -8,10 +8,10 @@ const {
 } = require("../middlewares/authMiddleware");
 router.post("/create", ProductController.createProduct);
 router.put("/update/:id", ProductController.updateProduct);
-router.get("/getAll", cors(), ProductController.getAllProduct);
+router.get("/getAll", ProductController.getAllProduct);
 router.get("/get/:id", ProductController.getProduct);
 router.delete("/delete/:id", ProductController.deleteProduct);
 router.post("/delete-many", authMiddleware, ProductController.deleteMany);
-router.get("/get-all-type", cors(), ProductController.getAllType);
+router.get("/get-all-type", ProductController.getAllType);
 router.get("/get-product-option", ProductController.getProductOption);
 module.exports = router;
